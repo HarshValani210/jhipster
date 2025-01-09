e222ad0e3a1de3d4d2a9227bd5882cde2858daf5
HarshValani210 <harshvalani20@gmail.com>
2025-01-09 11:15:17 +0530

    dd

--- src/main/webapp/app/entities/reducers.ts
+++ src/main/webapp/app/entities/reducers.ts
@@ -1,6 +1,10 @@
+import studentresult from 'app/entities/studentresult/studentresult.reducer';
+import student from 'app/entities/student/student.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 const entitiesReducers = {
+  studentresult,
+  student,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
 };
 
--- src/main/webapp/app/entities/routes.tsx
+++ src/main/webapp/app/entities/routes.tsx
@@ -3,6 +3,7 @@ import { Switch } from 'react-router-dom';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
 import Student from './student';
+import Studentresult from './studentresult';
 /* jhipster-needle-add-route-import - JHipster will add routes here */
 
 export default ({ match }) => {
@@ -11,6 +12,7 @@ export default ({ match }) => {
       <Switch>
         {/* prettier-ignore */}
         <ErrorBoundaryRoute path={`${match.url}student`} component={Student} />
+        <ErrorBoundaryRoute path={`${match.url}studentresult`} component={Studentresult} />
         {/* jhipster-needle-add-route-path - JHipster will add routes here */}
       </Switch>
     </div>
